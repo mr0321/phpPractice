72d01de9dfd68a77654cf7f700a5d994a73215ae
margus.ratsep <margus.ratsep@ttu.ee>
2023-09-22 15:10:36 +0300

    adds file upload

--- src/App/Config/Routes.php
+++ src/App/Config/Routes.php
@@ -5,7 +5,13 @@ declare(strict_types=1);
 namespace App\Config;
 
 use Framework\App;
-use App\Controllers\{HomeController, AboutController, AuthController, TransactionController};
+use App\Controllers\{
+  HomeController,
+  AboutController,
+  AuthController,
+  TransactionController,
+  ReceiptController
+};
 use App\Middleware\{AuthRequiredMiddleware, GuestOnlyMiddleware};
 
 function registerRoutes(App $app)
@@ -20,7 +26,11 @@ function registerRoutes(App $app)
   $app->get('/logout', [AuthController::class, 'logout'])->add(AuthRequiredMiddleware::class);
   $app->get('/transaction', [TransactionController::class, 'createView'])->add(AuthRequiredMiddleware::class);
   $app->post('/transaction', [TransactionController::class, 'create'])->add(AuthRequiredMiddleware::class);
-  $app->get('/transaction/{transaction}', [TransactionController::class, 'editView']);
-  $app->post('/transaction/{transaction}', [TransactionController::class, 'edit']);
-  $app->delete('/transaction/{transaction}', [TransactionController::class, 'delete']);
+  $app->get('/transaction/{transaction}', [TransactionController::class, 'editView'])->add(AuthRequiredMiddleware::class);
+  $app->post('/transaction/{transaction}', [TransactionController::class, 'edit'])->add(AuthRequiredMiddleware::class);
+  $app->delete('/transaction/{transaction}', [TransactionController::class, 'delete'])->add(AuthRequiredMiddleware::class);
+  $app->get('/transaction/{transaction}/receipt', [ReceiptController::class, 'uploadView'])->add(AuthRequiredMiddleware::class);
+  $app->post('/transaction/{transaction}/receipt', [ReceiptController::class, 'upload'])->add(AuthRequiredMiddleware::class);
+  $app->get('/transaction/{transaction}/receipt/{receipt}', [ReceiptController::class, 'download'])->add(AuthRequiredMiddleware::class);
+  $app->delete('/transaction/{transaction}/receipt/{receipt}', [ReceiptController::class, 'delete'])->add(AuthRequiredMiddleware::class);
 }
--- src/App/container-definitions.php
+++ src/App/container-definitions.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use Framework\{TemplateEngine, Database, Container};
 use App\Config\Paths;
-use App\Services\{ValidatorService, UserService, TransactionService};
+use App\Services\{ValidatorService, UserService, TransactionService, ReceiptService};
 
 return [
   TemplateEngine::class => fn () => new TemplateEngine(Paths::VIEW),
@@ -21,5 +21,10 @@ return [
   TransactionService::class => function (Container $container) {
     $db = $container->get(Database::class);
     return new TransactionService($db);
+  },
+  ReceiptService::class => function (Container $container) {
+    $db = $container->get(Database::class);
+
+    return new ReceiptService($db);
   }
 ];
